e5b17214b681df76208daf9f18fec8712a4b2d66
Jason Boileau <jason@boileau.dk>
2024-01-17 13:07:21 +0100

    Subscribe to weather sensor before temp sensors

--- src/mqttfan.py
+++ src/mqttfan.py
@@ -200,12 +200,12 @@ class MqttFanControl():
         self.mqttclient.subscribe(self.mqtt_state_topic)
         self.mqttclient.subscribe(self.mqtt_command_topic)
         self.mqttclient.subscribe(self.mqtt_mode_command_topic)
-        
-        for topic in self.mqtt_topic_map.keys():
-            self.mqttclient.subscribe(topic)
 
         if self.weather_topic:
             self.mqttclient.subscribe(self.weather_topic)
+
+        for topic in self.mqtt_topic_map.keys():
+            self.mqttclient.subscribe(topic)
 
         self.mqttclient.publish(self.availability_topic, payload='{"state": "online"}', qos=1, retain=True)
 
